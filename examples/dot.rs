3248ec90eaf73006bebab11dcff1ea0ce0ce7b36
VLAD EMINOVICI <veminovici@hotmail.com>
2021-09-09 19:44:38 +0300

    Update the examples

--- examples/dot.rs
+++ examples/dot.rs
@@ -10,18 +10,22 @@ fn main() {
     // Increment the dot
     dot1.apply_inc();
     eprintln!("Alice's incremented dot: {:?}", dot1);
+    assert_eq!(dot1.counter, 1, "Alice's dot must be 1");
 
     // Increment one more time the dot
     let dot11 = dot1.inc();
     eprintln!("Alice's incremented dot (again): {:?}", dot11);
+    assert_eq!(dot11.counter, 2, "Alice's dot must be 2");
 
     // Create a second dot.
-    let dot2 = Dot::new("Bob".to_string(), 0);
+    let dot2 = Dot::new("Bob".to_string(), 10);
     eprintln!("Bob's dot: {:?}", dot2);
+    assert_eq!(dot2.counter, 10, "Bob's dot must be 10");
 
     // Compare the two dot's
     let e = dot1.eq(&dot2);
     eprintln!("Are the dots comparable? {}", e);
+    assert_ne!(e, true, "The two dots cannot be compared");
 
     // Partial comparation alice's dots
     let e1 = dot11.partial_cmp(&dot1);
--- examples/vclock.rs
+++ examples/vclock.rs
@@ -6,11 +6,16 @@ fn main() {
     let mut vclock1 = VClock::new();
     vclock1.apply(vclock1.inc_op("A"));
     eprintln!("vclock a: {:?}", vclock1);
+    assert_eq!(vclock1.counter_of(&"A"), 1, "The clock for A must be 1");
 
     let mut vclock2 = VClock::new();
     vclock2.apply(vclock1.inc_op("B"));
     eprintln!("vclock b: {:?}", vclock2);
+    assert_eq!(vclock2.counter_of(&"B"), 1, "The clock for B must be 1");
 
     vclock1.merge(vclock2);
     eprintln!("vclock merged: {:?}", vclock1);
+    vclock1.inc("A");
+    assert_eq!(vclock1.counter_of(&"A"), 2, "The clock for A must be 2");
+    assert_eq!(vclock1.counter_of(&"B"), 1, "The clock for B must be 1");
 }
