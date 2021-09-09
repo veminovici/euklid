12553312b0a3eee576677c3d0dbd8424d79bb4c5
VLAD EMINOVICI <veminovici@hotmail.com>
2021-09-09 19:25:07 +0300

    Add gcounter example

---
+++ examples/gcounter.rs
@@ -0,0 +1,20 @@
+extern crate euklid;
+
+use euklid::{CmRDT, CvRDT, GCounter};
+
+fn main() {
+    let mut gcounter1 = GCounter::new();
+    gcounter1.inc("A");
+    eprintln!("gcounter 1: {:?}", gcounter1);
+
+    let mut gcounter2 = GCounter::new();
+    gcounter2.apply(gcounter1.inc_op("B"));
+    eprintln!("grounter 2: {:?}", gcounter2);
+
+    gcounter1.merge(gcounter2);
+    eprintln!("gcounter merged: {:?}", gcounter1);
+
+    gcounter1.stepup("A", 3);
+    gcounter1.inc("B");
+    eprintln!("gcounter updated: {:?}", gcounter1);
+}
