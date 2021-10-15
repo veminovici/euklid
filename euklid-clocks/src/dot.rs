56934834b11e9bbedc6160d555e530a707e7b997
Vlad Eminovici <veminovici@hotmail.com>
2021-10-15 18:19:34 +0300

    Created the clocks lib

---
+++ euklid-clocks/src/dot.rs
@@ -0,0 +1,5 @@
+/// A structure for the counter attached to an actor
+pub struct Dot<A> {
+    actor: A,
+    counter: u128,
+}
---
+++ euklid-clocks/src/lib.rs
@@ -0,0 +1,8 @@
+//! A rust library for CRDT clocks.
+
+#![deny(missing_docs)]
+#![deny(unreachable_pub)]
+
+mod dot;
+
+pub use crate::dot::Dot;
